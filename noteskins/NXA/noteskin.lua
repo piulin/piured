2eaa9da8cbd093e0692ceaa73f0839e5b7908699
Pedro Bascoy <pepo_gonba@hotmail.com>
2021-05-15 14:14:47 +0200

    Add sample song and noteskin

---
+++ noteskins/NXA/noteskin.lua
@@ -0,0 +1,9 @@
+return {
+	notes= "notes.lua",
+	layers= {"receptors.lua", "explosions.lua"},
+	supports_all_buttons= false,
+	buttons= {"UpLeft", "UpRight", "DownLeft", "DownRight", "Center"},
+	fallback= "NX2",
+	skin_parameters= {},
+	skin_parameter_info= {},
+}
