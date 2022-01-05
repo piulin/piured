0fd3677da1c7ba5069d041a799d0de19202492cc
Pedro Bascoy <pepo_gonba@hotmail.com>
2022-01-05 18:29:44 +0100

    remove engine sources

--- noteskins/NXA/noteskin.lua
+++
@@ -1,9 +0,0 @@
-return {
-	notes= "notes.lua",
-	layers= {"receptors.lua", "explosions.lua"},
-	supports_all_buttons= false,
-	buttons= {"UpLeft", "UpRight", "DownLeftStepNote", "DownRight", "Center"},
-	fallback= "NX2",
-	skin_parameters= {},
-	skin_parameter_info= {},
-}
--- shaders/background.vert
+++
@@ -1,8 +0,0 @@
-varying vec4 vModelPosition;
-varying vec2 vUv;
-
-void main() {
-    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-    vUv = uv ;
-    vModelPosition = modelMatrix * vec4( position, 1.0 );
-}
